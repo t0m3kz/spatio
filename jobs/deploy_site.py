6d8d6e0ddab308a0bec9822219dca4541cfaa756
Tomek Zajac <tomek@mastuazi.com>
2023-10-14 23:40:40 +0200

    update

--- jobs/deploy_site.py
+++ jobs/deploy_site.py
@@ -1,70 +1,37 @@
-from django.utils.text import slugify
-
-from nautobot.dcim.models import Device, DeviceRole, DeviceType, Manufacturer, Site
-from nautobot.extras.models import Status
-from nautobot.extras.jobs import *
+"""Branch Job"""
+from nautobot.core.celery import register_jobs
+from nautobot.extras.jobs import Job, StringVar
+from nautobot.dcim.models import Location
 
 
 class NewBranch(Job):
+    """
+    System job to clone and/or pull a Git repository, then invoke `refresh_datasource_content()`.
+    """
 
     class Meta:
+        """Meta class."""
+
         name = "New Branch"
         description = "Provision a new branch site"
-        field_order = ['site_name', 'switch_count', 'switch_model']
+        field_order = ["site_name", "switch_count", "switch_model"]
+        site_name = StringVar(description="Name of the new site")
 
-    site_name = StringVar(
-        description="Name of the new site"
-    )
-    switch_count = IntegerVar(
-        description="Number of access switches to create"
-    )
-    manufacturer = ObjectVar(
-        model=Manufacturer,
-        required=False
-    )
-    switch_model = ObjectVar(
-        description="Access switch model",
-        model=DeviceType,
-        query_params={
-            'manufacturer_id': '$manufacturer'
-        }
-    )
+    def run(self, data):
+        """Execute Job."""
 
-    def run(self, data, commit):
-        STATUS_PLANNED = Status.objects.get(slug='planned')
+        job_result = self.job_result
+        self.logger.info("Creating new branch office...")
 
-        # Create the new site
-        site = Site(
-            name=data['site_name'],
-            slug=slugify(data['site_name']),
-            status=STATUS_PLANNED,
-        )
-        site.validated_save()
-        self.log_success(obj=site, message="Created new site")
-
-        # Create access switches
-        switch_role = DeviceRole.objects.get(name='Access Switch')
-        for i in range(1, data['switch_count'] + 1):
-            switch = Device(
-                device_type=data['switch_model'],
-                name=f'{site.slug}-switch{i}',
-                site=site,
-                status=STATUS_PLANNED,
-                device_role=switch_role
+        try:
+            site = Location(
+                name=data["site_name"],
             )
-            switch.validated_save()
-            self.log_success(obj=switch, message="Created new switch")
+            site.validated_save()
+        finally:
+            self.logger.info(
+                f"Repository synchronization completed in {job_result.duration}"
+            )
 
-        # Generate a CSV table of new devices
-        output = [
-            'name,make,model'
-        ]
-        for switch in Device.objects.filter(site=site):
-            attrs = [
-                switch.name,
-                switch.device_type.manufacturer.name,
-                switch.device_type.model
-            ]
-            output.append(','.join(attrs))
 
-        return '\n'.join(output)
+register_jobs(NewBranch)
