bed47afa1acf3a0d035b49be4107213ccbdd3751
Tomek Zajac <tomek@mastuazi.com>
2023-10-15 11:37:29 +0200

    test

--- jobs/deploy_site.py
+++ jobs/deploy_site.py
@@ -10,14 +10,14 @@ class NewBranch(Job):
     """
     System job to deploy a new branch office """   
     site_name = StringVar(description="Name of the new site")
-
+    country_name = StringVar(description="Country of the new site")
     class Meta:
         """Meta class."""
         name = "New Branch"
         description = "Provision a new branch site"
         has_sensitive_variables = False
 
-    def run(self, site_name):
+    def run(self, site_name, country_name):
         """Execute Job."""
 
         job_result = self.job_result
@@ -30,7 +30,8 @@ class NewBranch(Job):
             site = Location(
                 name=site_name,
                 location_type=LocationType.objects.filter(name="Site").get(),
-                status=Status.objects.filter(name="Active").get()
+                status=Status.objects.filter(name="Active").get(),
+                parent=Location.objects.filter(name=country_name).get(),
             )
             site.validated_save()
         finally:
