86603448c7b8300533072093b1f7afb13ce40f4b
Tomek Zajac <tomek@mastuazi.com>
2023-10-15 10:55:58 +0200

    fix

--- jobs/deploy_site.py
+++ jobs/deploy_site.py
@@ -25,7 +25,7 @@ class NewBranch(Job):
         try:
             site = Location(
                 name=site_name,
-                location_type=LocationType.objects.filter(name="Site").id,
+                location_type=LocationType.objects.filter(name="Site").name,
                 status="Active",
             )
             site.validated_save()
