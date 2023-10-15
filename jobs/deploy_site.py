e8a951b388544e302e50a8cab2bbed32a1252f05
Tomek Zajac <tomek@mastuazi.com>
2023-10-15 15:54:56 +0200

    update

--- jobs/deploy_site.py
+++ jobs/deploy_site.py
@@ -1,4 +1,6 @@
 """Branch Job"""
+import time
+import datetime
 import requests
 from nautobot.extras.jobs import Job, StringVar
 from nautobot.dcim.models import Location, LocationType
@@ -47,9 +49,32 @@ class NewBranch(Job):
                 "ref": "main",
                 "inputs": {"data": "deploy_branch"},
             }
-            response = requests.post(url, headers=headers, json=data)
+            response = requests.post(url, headers=headers, json=data, timeout=10)
             if response.status_code == 204:
                 self.logger.info(f"Site {site_name} in {city_name} is being deployed.")
+                workflows_url = (
+                    f"https://api.github.com/repos/{owner}/{repo}/actions/runs"
+                )
+                # Let's wait a bit for the workflow to start
+                time.sleep(2)
+                # Wait until the workflow is completed
+                start_time = datetime.datetime.now()
+                while True:
+                    response = requests.get(workflows_url, headers=headers, timeout=10)
+                    if response.status_code == 200:
+                        workflow = response.json()["workflow_runs"][0]
+                        if workflow["conclusion"] is not None:
+                            self.logger.info(
+                                f"Deployment completed in {datetime.datetime.now() - start_time}"
+                            )
+                            break
+                        else:
+                            self.logger.info("Workflow still running. Waiting...")
+                            print("Workflow still running. Waiting...")
+                            time.sleep(5)
+                    else:
+                        print("Failed to get workflow status.")
+                        break
             else:
                 self.logger.error(
                     f"Site {site_name} in {city_name} cannot be deployed. {response.status_code}"
