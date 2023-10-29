22cb31ea85bdbff3ffdf7c35fbb6651f5dd8fa95
Tomek Zajac <tomek@mastuazi.com>
2023-10-29 16:35:29 +0100

    test fields

--- jobs/deploy_tenant.py
+++ jobs/deploy_tenant.py
@@ -2,12 +2,30 @@
 import time
 import datetime
 import requests
+from django import forms
 from nautobot.extras.jobs import Job, StringVar, ObjectVar, BooleanVar, ChoiceVar
 from nautobot.dcim.models import Location
 from nautobot.ipam.models import Namespace
 from nautobot.extras.models import Status
 
 name = "ACI Jobs"
+
+BIRTH_YEAR_CHOICES = ["1980", "1981", "1982"]
+FAVORITE_COLORS_CHOICES = [
+    ("blue", "Blue"),
+    ("green", "Green"),
+    ("black", "Black"),
+]
+
+class SimpleForm(forms.Form):
+    birth_year = forms.DateField(
+        widget=forms.SelectDateWidget(years=BIRTH_YEAR_CHOICES)
+    )
+    favorite_colors = forms.MultipleChoiceField(
+        required=False,
+        widget=forms.CheckboxSelectMultiple,
+        choices=FAVORITE_COLORS_CHOICES,
+    )
 
 
 class NewTenant(Job):
@@ -47,6 +65,9 @@ class NewTenant(Job):
     )
 
     deployment = ChoiceVar(choices=CHOICES)
+
+    something_new = SimpleForm()
+
 
     class Meta:
         """Meta class."""
