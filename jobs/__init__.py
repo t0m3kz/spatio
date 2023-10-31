955bf8b5cdec81696b99435fd163d53ce633d73c
Tomek Zajac <tomek@mastuazi.com>
2023-10-31 11:31:20 +0100

    fixed python

--- jobs/__init__.py
+++ jobs/__init__.py
@@ -2,6 +2,7 @@
 from nautobot.core.celery import register_jobs
 from .deploy_site import NewBranch
 from .deploy_tenant import NewTenant
+from .deploy_epg import NewEpg
 
-jobs = [NewBranch, NewTenant]
+jobs = [NewBranch, NewTenant, NewEpg]
 register_jobs(*jobs)
