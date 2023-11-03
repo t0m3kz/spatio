3964a7724894862856abaaf1b6ddc9043cebad1d
Tomek Zajac <tomek@mastuazi.com>
2023-11-03 11:40:57 +0100

    update

--- jobs/__init__.py
+++ jobs/__init__.py
@@ -5,5 +5,5 @@ from .deploy_tenant import NewTenant
 from .deploy_epg import NewEpg
 from .deploy_bd import NewBd
 
-jobs = [NewBranch, NewTenant, NewEpg]
+jobs = [NewBranch, NewTenant, NewEpg, NewBd]
 register_jobs(*jobs)
